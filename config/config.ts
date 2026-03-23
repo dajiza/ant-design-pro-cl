130aae6a15015d768ec66cccae9ab75edc235e53
sa.chen <chensa222@gmail.com>
2026-03-23 14:32:27 +0800

    chore: initial commit

--- config/config.ts
+++ config/config.ts
@@ -17,6 +17,7 @@ const { UMI_ENV = 'dev' } = process.env;
 const PUBLIC_PATH: string = '/';
 
 export default defineConfig({
+  port: 3018,
   /**
    * @name 开启 hash 模式
    * @description 让 build 之后的产物包含 hash 后缀。通常用于增量发布和避免浏览器加载缓存。
